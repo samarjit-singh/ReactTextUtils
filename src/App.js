1879e34b7dbd7d445548e8c61e05c4baad898263
samarjit-singh <75423063+samarjit-singh@users.noreply.github.com>
2022-02-17 17:45:43 +0530

    remove space nad copy text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <Navbar title="TextUtils" />
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze below" />
-        <About />
+        {/* <About /> */}
       </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,6 +18,18 @@ export default function TextForm(props) {
     // console.log("On Change");
     setText(event.target.value);
   };
+
+  const handleCopy = (event) => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
   const [text, setText] = useState("");
 
   return (
@@ -41,6 +53,12 @@ export default function TextForm(props) {
         </button>
         <button className="btn btn-primary mx-2" onClick={handleClearText}>
           Clear Text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleCopy}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>
+          Remove Extra Spaces
         </button>
       </div>
       <div className="container my-3">
